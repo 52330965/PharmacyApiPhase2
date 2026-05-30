8cd632c5a038de2e7b9046957e80f4d89f35349d
rola <52330965@students.liu.edu.lb>
2026-05-30 16:19:21 +0300

    create  Data folder contain class dbcontext

---
+++ PharmacyProjectPhase2/Data/PharmacyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using PharmacyProjectPhase2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyProjectPhase2.Data
+{
+    public class PharmacyDbContext:DbContext
+    {
+        public PharmacyDbContext(DbContextOptions<PharmacyDbContext>options):base(options)
+            { 
+
+        }
+        public DbSet<Medicines> Medicines { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PharmacyProjectPhase2/Models/Medicines.cs
@@ -0,0 +1,14 @@
+﻿namespace PharmacyProjectPhase2.Models
+{
+    public class Medicines
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public int Quantity { get; set; }
+        public double  Price { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+    }
+}
