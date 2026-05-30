d351806ff45bfd8750340231cdace3aa6d3a5725
rola <52330965@students.liu.edu.lb>
2026-05-30 16:15:37 +0300

    git commit -m "create models file contain 2 classes"

---
+++ PharmacyProjectPhase2/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyProjectPhase2.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+
+        [Required,MaxLength(10)]
+        public string Password { get; set; }
+    }
+}
